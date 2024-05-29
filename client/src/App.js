799467fbd04705356a0a2aaa8bbe0353ef25ce5b
pgm-benobira <benoit.biragumaihimbazwe@student.arteveldehs.be>
2024-05-30 00:51:33 +0200

    ✨ Set up graphcms-boilerplate

---
+++ client/src/App.js
@@ -0,0 +1,28 @@
+import './assets/styles/reset.css'
+import './assets/styles/App.css';
+import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements, } from "react-router-dom";
+import { ROUTES } from "./routes/routes";
+import Root from "./layouts/Root";
+import { Home, Study, Program, Projects, Project, Blog, BlogPost, Services, Team, Error } from "./pages";
+
+const router = createBrowserRouter(createRoutesFromElements(
+  <Route path="/" element={<Root />} errorElement={<Error />}>
+    <Route path={ROUTES.home.path} element={<Home />} />
+    <Route path={ROUTES.study.path} element={<Study />} />
+    <Route path={ROUTES.program.path} element={<Program />} />
+    <Route path={ROUTES.projects.path} element={<Projects />} />
+    <Route path={ROUTES.project.path} element={<Project />} />
+    <Route path={ROUTES.blog.path} element={<Blog />} />
+    <Route path={ROUTES.blogPost.path} element={<BlogPost />} />
+    <Route path={ROUTES.services.path} element={<Services />} />
+    <Route path={ROUTES.team.path} element={<Team />} />
+  </Route>
+));
+
+function App() {
+  return (
+    <RouterProvider router={router} />
+  );
+}
+
+export default App;
