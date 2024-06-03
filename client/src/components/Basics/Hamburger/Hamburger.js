75cdc2a2702f1f1660b65ad80460df94d9fa5660
pgm-benobira <benoit.biragumaihimbazwe@student.arteveldehs.be>
2024-06-03 14:07:17 +0200

    🍱 Added Hamburger component and ✨ Mobile menu in the header

---
+++ client/src/components/Basics/Hamburger/Hamburger.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import './Hamburger.css'
+
+export default function Hamburger() {
+  return (
+    <button className='hamburger-icon'>
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 25.625 16.625"><path fill="none" stroke="#ff6e1f" strokeLinecap="round" strokeMiterlimit="10" strokeWidth="2" d="M1 1h23.625M1 8.312h23.625M1 15.625h23.625" dataName="Icon ion-menu"/></svg>
+    </button>
+  )
+}
--- client/src/components/Elements/Header/Header.js
+++ client/src/components/Elements/Header/Header.js
@@ -1,20 +1,34 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import styles from './Header.module.css'
 
 import Logo from '../Logo'
 import Navigation from '../Navigation'
-import Button from '../../Basics/Button/Button'
-import ThemeSwitcher from '../../Basics/ThemeSwitcher/ThemeSwitcher'
+import Button from '../../Basics/Button'
+import ThemeSwitcher from '../../Basics/ThemeSwitcher'
+import Hamburger from '../../Basics/Hamburger'
 
 export default function Header() {
+  const [menuOpen, setMenuOpen] = useState(false)
+  const toggleMenu = () => setMenuOpen(!menuOpen);
+
   return (
     <header className={styles.header}>
         <Logo />
-        <Navigation />
-        <ThemeSwitcher />
-        <Button text="Inloggen" type='secondary' />
-        <Button text="Inschrijven" link="https://www.arteveldehogeschool.be/node/3854" target='blank' />
+        <div className={styles.animation}>
+          <Button type='secondary' />
+        </div>
+        <div className={styles.hamburger} onClick={toggleMenu}>
+          {menuOpen ? 'x' : <Hamburger/>}
+        </div>
+        <div className={`${styles.operations} ${menuOpen ? styles.open : ''}`}>
+          <Navigation />
+          <ThemeSwitcher />
+          <div className={styles.actions}>
+            <Button text="Inloggen" type='secondary' />
+            <Button text="Inschrijven" link="https://www.arteveldehogeschool.be/node/3854" target='blank' />
+          </div>
+        </div>
     </header>
   )
 }
